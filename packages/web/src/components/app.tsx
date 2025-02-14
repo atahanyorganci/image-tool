fe7469a3213c921d21ce7968956626d6293fb4d8
Atahan Yorgancı <atahanyorganci@hotmail.com>
2025-02-15 01:40:45 +0300

    feat: add dropzone for image upload using `react-dropzone`

--- packages/web/src/components/app.tsx
+++ packages/web/src/components/app.tsx
@@ -1,11 +1,36 @@
 import type { FC } from "react";
 import React from "react";
+import { useDropzone } from "react-dropzone";
+import { Button } from "./button";
+
+const Dropzone: FC = () => {
+	const { getRootProps, getInputProps, isDragActive } = useDropzone({
+		multiple: false,
+		onDrop: (files) => {
+			console.log(files);
+		},
+	});
+
+	return (
+		<div
+			{...getRootProps()}
+			className="max-w-5xl cursor-pointer w-full mx-auto flex-1 m-24 rounded-2xl flex flex-col items-center justify-center border-2 border-dashed border-white"
+		>
+			<input {...getInputProps()} accept="image/*" />
+			{isDragActive && <p>Drop here</p>}
+			{!isDragActive && (
+				<div className="flex flex-col gap-6 items-center">
+					<h1 className="text-2xl font-medium">Choose an image or Drag 'n' Drop.</h1>
+					<Button size="lg" className="max-w-fit">Choose a File</Button>
+				</div>
+			)}
+		</div>
+	);
+};
 
 const App: FC = () => (
-	<div>
-		<h1 className="text-7xl font-bol tracking-wide">
-			Hello World
-		</h1>
+	<div className="bg-background text-foreground min-h-screen flex flex-col w-full">
+		<Dropzone />
 	</div>
 );
 
