a1fa0ec6a8b5ab706f2888c024f81258343bc3c3
Atahan Yorgancı <atahanyorganci@hotmail.com>
2025-06-08 16:40:39 +0300

    feat: add `@yorganci/eslint-plugin-tailwindcss`

--- packages/eslint-config/src/index.ts
+++ packages/eslint-config/src/index.ts
@@ -1,6 +1,7 @@
 import type { Linter } from "eslint";
 import type { FlatConfigComposer } from "eslint-flat-config-utils";
 import antfu from "@antfu/eslint-config";
+import tailwindcss from "@yorganci/eslint-plugin-tailwindcss";
 
 /**
  * @description Options for the configuration.
@@ -10,13 +11,17 @@ export interface Options {
 	 * @description Whether the configuration is for a web project.
 	 */
 	web: boolean;
+	/**
+	 * @description TailwindCSS stylesheet
+	 */
+	tailwindStylesheet: string;
 }
 
 /**
  * @description This configuration is based on the `@antfu/eslint-config` configuration with some additional rules.
  */
-export function imageTool({ web }: Partial<Options> = {}): FlatConfigComposer<Linter.Config<Linter.RulesRecord>, never> {
-	return antfu({
+export function imageTool({ web, tailwindStylesheet }: Partial<Options> = {}): FlatConfigComposer<Linter.Config<Linter.RulesRecord>, never> {
+	let config = antfu({
 		react: web,
 		typescript: true,
 		formatters: true,
@@ -30,4 +35,20 @@ export function imageTool({ web }: Partial<Options> = {}): FlatConfigComposer<Li
 			"eslint-comments/no-unlimited-disable": ["off"],
 		},
 	});
+	if (tailwindStylesheet) {
+		config = config.append({
+			plugins: {
+				tailwindcss,
+			},
+			settings: {
+				tailwindcss: {
+					stylesheet: tailwindStylesheet,
+				},
+			},
+			rules: {
+				"tailwindcss/classname-order": "error",
+			},
+		});
+	}
+	return config;
 }
--- packages/web/eslint.config.ts
+++ packages/web/eslint.config.ts
@@ -2,6 +2,7 @@ import { imageTool } from "@yorganci/image-tool-eslint-config";
 
 const config = imageTool({
 	web: true,
+	tailwindStylesheet: "src/styles/global.css",
 });
 
 export default config;
